5e4a96dc31d22e3f9c134410a3c63377ac6f5bb5
Pranay <deshmukhpranay772@gmail.com>
2021-07-16 23:57:17 +0530

    Updates the files.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var check_amount = document.getElementById("principal"); 
+    if (check_amount.value <= 0)    //Checking the value is positive or not 
+    {
+        alert("Please Enter a positive number!");
+        return false;
+    }
+    else {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        var amount = document.getElementById("result").innerHTML = "\<br\>If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+        var check_amount = document.getElementById("principal");
+
+    }
+
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
